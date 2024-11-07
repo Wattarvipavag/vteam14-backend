6057bc9572908a771b6d0b5463d76e316ba82497
Jakob Eriksson <jkberiksson@gmail.com>
2024-11-07 15:03:54 +0100

    Create express server

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello world!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
