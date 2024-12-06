c0a4ffbcf98b7ba181964fe756ff159ac2218c3d
Patrik Arvius <patrik.arvius@gmail.com>
2024-12-06 14:01:08 +0100

    Add getUserOauth function

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,26 @@ export async function getUser(req, res) {
         const user = await User.findById(userId);
 
         if (!user) {
-            return res.status(404).json({ message: `No user found with ${userId}` });
+            return res
+                .status(404)
+                .json({ message: `No user found with ${userId}` });
+        }
+
+        return res.status(200).json({ message: 'User exists', user: user });
+    } catch (e) {
+        res.status(500).json({ message: e.message });
+    }
+}
+
+export async function getUserOauth(req, res) {
+    try {
+        const oauthId = req.params.id;
+        const user = await User.findOne({ oauthId });
+
+        if (!user) {
+            return res
+                .status(404)
+                .json({ message: `No user found with oauth id: ${oauthId}` });
         }
 
         return res.status(200).json({ message: 'User exists', user: user });
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { getAllUsers, getUser } from '../controllers/userController.js';
+import {
+    getAllUsers,
+    getUser,
+    getUserOauth,
+} from '../controllers/userController.js';
 
 const router = Router();
 
 router.get('/', getAllUsers);
 router.get('/:id', getUser);
+router.get('/oath/:id', getUserOauth);
 
 export default router;
